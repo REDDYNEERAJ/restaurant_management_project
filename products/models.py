8ab3d12b22dd504ac4c15086eade4fdb8ae591c5
s0m1th <r.reddysomith@gmailcom>
2025-09-16 20:59:05 +0530

    Show Only Logged-in User’s Notes

--- products/models.py
+++ products/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
 class Item(models.Model):
     item_name = models.CharField(max_length=150)
     item_price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="items")
 
     def __str__(self):
         return str(self.item_name)
--- products/views.py
+++ products/views.py
@@ -2,25 +2,23 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Item
 from .serializers import ItemSerializer
 
-'''
-NOTE: Conside this as a reference and follow this same coding structure or format to work on you tasks
-'''
-
 # Create your views here.
 class ItemView(APIView):
+    permission_classes = [IsAuthenticated]  # Only logged-in users can access
 
     def get(self, request):
-        items = Item.objects.all()
+        items = Item.objects.filter(user=request.user)  # Filter by logged-in user
         serializer = ItemSerializer(items, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
         serializer = ItemSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(user=request.user)  # Assign logged-in user automatically
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
