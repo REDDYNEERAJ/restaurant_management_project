7fff56376313cfdb3ebe8446531b943fc158f59f
REDDY NEERAJ R <103424274+REDDYNEERAJ@users.noreply.github.com>
2025-09-21 13:27:12 +0530

    49774

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import MenuCategory
 
 # Register your models here.
+@admin.register(MenuCategory)
+class MenuCategoryAdmin(admin.ModelAdmin):
+    list_display = ('id','name')
+    search_fields = ('name',)
