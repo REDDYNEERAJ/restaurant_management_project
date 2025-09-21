03067d15c2a40d90ba0044d5568283d9ab50d880
REDDY NEERAJ R <103424274+REDDYNEERAJ@users.noreply.github.com>
2025-09-21 12:55:40 +0530

    49774

--- home/models.py
+++ home/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
