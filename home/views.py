f12aec561d5b1d20705cf95566709f514af0f113
REDDY NEERAJ R <103424274+REDDYNEERAJ@users.noreply.github.com>
2025-09-21 13:22:19 +0530

    49774

--- home/views.py
+++ home/views.py
@@ -2,9 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from .models import MenuCategory
 from .serializers import MenuCategorySerializer
 
 class MenuCategoryListView(ListAPIView):
     queryset = MenuCategory.objects.all()
     serializer_class = MenuCategorySerializer
+    permission_classes = [AllowAny]
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def MenuCategory_func_view(request):
+    categories = MenuCategory.all()
+    serializer = MenuCategorySerializer(categories,many=True)
+    return Response(serializer.data)
